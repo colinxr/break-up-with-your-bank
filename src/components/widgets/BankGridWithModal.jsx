3b655035d6a56ced9f05e5da4f1e1697a7269f5b
colin-as <colin@halfhelix.com>
2024-06-07 17:11:06 -0400

    clean up index template

--- src/components/widgets/BankGridWithModal.jsx
+++ src/components/widgets/BankGridWithModal.jsx
@@ -33,7 +33,7 @@ const BankGridWithModal = ({ items = [], columns, classes = {} }) => {
           )}
           {
             cta && (
-              <a href={cta} class="inline-flex mt-5 self-end items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2">Read More</a>
+              <a href={cta} class="inline-flex text-white mt-5 self-start items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2">Read More</a>
             )
           }
         </div>
