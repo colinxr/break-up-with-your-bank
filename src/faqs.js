550ef68cd35c82fafb732d3eae4a8a3c0d94e351
Colin Rabyniuk <colin.rabyniuk@meetdomaine.com>
2024-10-06 11:23:47 -0700

    alignment on homepage

--- src/faqs.js
+++ src/faqs.js
@@ -1,30 +1,26 @@
 export default [ 
-      {
-        title: 'What is a credit union?',
-        description:
-          '<p>Credit unions (or caisse populaires as they are called in Quebec) are member-owned,  cooperative financial institutions. This means they’re owned and controlled by the people who use them, offering pathways for accountability unavailable at banks. Some credit unions return profits to members as dividends, reduced fees, higher savings rates, or lower loan rates. Profits are also frequently donated or invested into local community initiatives.</p><p class="mt-3">At credit unions, you’re a member/shareholder, not just a customer. This means you get a say in some aspects of how the institution is run, including an equal vote (regardless of your account or investment sizes) to elect the board of directors. This democratic process and the explicit community focus of credit unions generally make them a more ethical alternative, while offering many of the same products and services (personal and business) as big banks.',
-      },
-      {
-        title: 'Is my money safe with a credit union?',
-        description:
-          '<p>Yes, your money is protected and government-insured, just like at a bank. Look for the CDIC logo and know that your money is safe.  If you don’t see the logo, ask your representative about what insurance the institution has. </p><p class="mt-3">Having a chequing and/or savings account with a credit union is not any riskier than at a Big 5 bank. <blockquote class="mt-3 pl-4 border-l-2">Credit unions in Canada are either provincially or federally regulated. Regulators have oversight over individual credit unions; and credit unions are required to meet standards and work with public agencies to ensure they are among the country’s soundest financial institutions. - CCUA</blockquote>',
-      },
-      {
-        title: 'Can I expect the same services at a credit union?',
-        description:
-          '<p>No, not necessarily.  Some credit unions are smaller, more local with less services and others have branches in multiple provinces and the services provided will vary with each institution.  You will have to check to determine what services the credit union has. Do you need: <ul class="mt-3 pl-4 ml-2 list-disc" ><li>Online Services</li><li>Credit Card</li><li>Investment Options</li><li>Mortgages</li><li>Perks (e.g. travel points)</li><li>Investment Options</li></li><li>Physical Location</li></ul><p class="mt-3">Take a look at the fees.  Some banking institutions have minimum deposit amounts for fees to be waived. <p class="mt-3" >How many transactions (money coming and going) do you do each month? Some have a limited number of transactions for free accounts. Some offer free and unlimited e-transfers',
-      },
-      {
-        title: 'Will I still have access to ATMs if I switch to a credit union?',
-        description:
-          '<p>Yes, most credit unions are part of a network that allows their members to access a large number of ATMs nationwide. Additionally, many credit unions reimburse a certain amount of ATM fees incurred by their members each month. It’s always a good idea to check with the credit union you are considering to see what ATM options and fee reimbursements they offer',
-      },
-      {
-        title: 'Where do I learn more about this issue?',
-        description: '<p>Check out the amazing work these organizations are doing to help you take control of your money! <ul class="mt-3 pl-4 ml-2 list-disc"><li><a href="https://www.gabv.org/" class="underline">Global Alliance for Banking on Values</a> is a network of independent banks using finance to deliver sustainable economic, social and environmental development. Find out if your financial institution is a member <a href="https://www.gabv.org/members/">here</a>.</li><li class="mt-2"><a href="https://www.banktrack.org/banks" class="underline">BankTrack</a> is an international organization that tracks financial institutions’ involvement with companies and projects that have a negative impact on people and the plant. Follow the link to find out if your bank is involved in any ‘dodgy deals’.</li> <li class="mt-2">Search <a href="https://weaponfreefunds.org/" class="underline">Weapons Free Funds</a> to find out if you’re investing in militarism.</li><li class="mt-2"><a href="https://linktr.ee/decolonialsolidarity" class="underline">Decolonial Solidarity</a> is a Canadian organization, comprised of Indigenous and allied supporters calling on RBC to end its funding of the Coastal GasLink Pipeline on Wet’suwet’en land.</li> <li class="mt-2">Change Course’s <a href="https://changecourse.ca/our-work/banks-off-campus/" class="underline">Banks off Campus</a> campaign supports grassroots organizers on university and college campuses to get big banks off campus by focusing on student unions to divest from big banks. </li><li class="mt-2"><a href="https://www.justpeaceadvocates.ca/" class="underline">Just Peace Advocates</a> is comprised of groups in Canada that support the global Boycott Israel movement targeting Israel’s system of settler-colonialism, occupation, and apartheid towards the Palestinian people.</li></ul>'
-      },
+  {
+    title: 'Is my money safe with a credit union?',
+    description:
+      '<p>Yes! Credit unions are regulated and insured in the same way as banks. The difference is that banks are federally insured, while the majority of credit unions are provincially insured. This actually works in your favour, because the federal government only insures your account to a limit of $100k, whereas provincial limits are $250k. This means that your credit union account is insured to a higher amount than your bank account! Look for your provinces deposit insurance or guarantee (DICO in Ontario) for more information.</p>',
+  },
+  {
+    title: 'Can I expect the same services at a credit union?',
+    description:
+      '<p>Credit unions offer nearly identical services as banks. You’ll often experience more personalized customer service, shorter wait times on the phone, and less employee turnover, meaning that the person you’re talking to is likely to have more knowledge and experience than a teller at the bank!',
+  },
   {
     title: 'What about my mortgage/debt/lines of credit?',
-    description: 'Don’t let worries about your debts or loans hold you back from making the switch. It is absolutely doable to switch your mortgage, loans, lines of credit- but it isn’t critical! Your everyday banking and investments are the money your bank counts on using to their benefit. You can open a new account at a credit union, and still continue to make payments through your bank to your mortgage, line of credit etc.',
-  }
-    ]
+    description:
+      '<p>While it’s certainly possible to switch your mortgages, loans and investments to a credit union, you don’t have to take these steps in order to make an impact! The money in your chequing and savings accounts are what the bank uses to fund their unsavory investments. They cannot leverage your debt in the same way. We suggest switching your chequing and savings accounts to a credit union, and keeping your mortgages, loans, lines of credit, etc. where they are. We encourage you to shop around for a credit card that best suits your needs- that may mean continuing to use a bank-associated card, or signing up with an independent one. You will still be able to make all of your regular payments through your credit union account.',
+  },
+  {
+    title: 'Will I still have access to ATMs if I switch to a credit union?',
+    description:
+      '<p>Yes! Most credit unions are part of a large national network that allows users to access ATMs surcharge-free nationwide. Visit <a href="https://dingfree.ca">dingfree.ca</a> or <a href="https://exchangenetwork.ca">exchangenetwork.ca</a> to find a connected ATM near you!</p>',
+  },
+  {
+    title: 'Where do I learn more?',
+    description: '<p>Check out the amazing work these organizations are doing to help you take control of your money! <ul class="mt-3 pl-4 ml-2 list-disc"><li><a href="https://www.gabv.org/" class="underline">Global Alliance for Banking on Values</a> is a network of independent banks using finance to deliver sustainable economic, social and environmental development. Find out if your financial institution is a member <a href="https://www.gabv.org/members/">here</a>.</li><li class="mt-2"><a href="https://www.banktrack.org/banks" class="underline">BankTrack</a> is an international organization that tracks financial institutions’ involvement with companies and projects that have a negative impact on people and the plant. Follow the link to find out if your bank is involved in any ‘dodgy deals’.</li> <li class="mt-2">Search <a href="https://weaponfreefunds.org/" class="underline">Weapons Free Funds</a> to find out if you’re investing in militarism.</li><li class="mt-2"><a href="https://linktr.ee/decolonialsolidarity" class="underline">Decolonial Solidarity</a> is a Canadian organization, comprised of Indigenous and allied supporters calling on RBC to end its funding of the Coastal GasLink Pipeline on Wet’suwet’en land.</li> <li class="mt-2">Change Course’s <a href="https://changecourse.ca/our-work/banks-off-campus/" class="underline">Banks off Campus</a> campaign supports grassroots organizers on university and college campuses to get big banks off campus by focusing on student unions to divest from big banks. </li><li class="mt-2"><a href="https://www.justpeaceadvocates.ca/" class="underline">Just Peace Advocates</a> is comprised of groups in Canada that support the global Boycott Israel movement targeting Israel’s system of settler-colonialism, occupation, and apartheid towards the Palestinian people.</li></ul>'
+  },
+]
